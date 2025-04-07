20b8756cadf910255b2b53e2d7c5d521b64cefe6
Ariel correa maturana <arielcorreama@gmail.com>
2025-04-07 15:49:52 -0500

    Add files via upload

---
+++ sistema.sql
@@ -0,0 +1,460 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-04-2025 a las 21:17:51
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DesbloquearUsuario` (IN `usuario_id` INT)   BEGIN
+    UPDATE empleados
+    SET bloqueado = FALSE, intentos_fallidos = 0
+    WHERE id = usuario_id;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auditoria`
+--
+
+CREATE TABLE `auditoria` (
+  `id` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha` datetime DEFAULT current_timestamp(),
+  `usuarios_id` int(11) DEFAULT NULL,
+  `direccion_ip` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `direccion` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_compra`
+--
+
+CREATE TABLE `detalles_compra` (
+  `id` int(11) NOT NULL,
+  `compra_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_ventas`
+--
+
+CREATE TABLE `detalles_ventas` (
+  `cliente_id` int(11) NOT NULL,
+  `venta_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `puesto` varchar(100) DEFAULT NULL,
+  `rol` enum('Cliente','Vendedor','Administrador') NOT NULL,
+  `intentos_fallidos` int(11) DEFAULT 0,
+  `bloqueado` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contacto` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_compras`
+--
+
+CREATE TABLE `registro_compras` (
+  `id` int(11) NOT NULL,
+  `proveedor_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles_permisos`
+--
+
+CREATE TABLE `roles_permisos` (
+  `id` int(11) NOT NULL,
+  `rol` enum('Cliente','Vendedor','Administrador') NOT NULL,
+  `permiso` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles_permisos`
+--
+
+INSERT INTO `roles_permisos` (`id`, `rol`, `permiso`) VALUES
+(1, 'Cliente', 'Ver productos'),
+(2, 'Cliente', 'Realizar compras'),
+(3, 'Cliente', 'Ver historial'),
+(4, 'Vendedor', 'Gestionar productos'),
+(5, 'Vendedor', 'Registrar ventas'),
+(6, 'Administrador', 'Gestionar usuarios'),
+(7, 'Administrador', 'Ver reportes'),
+(8, 'Administrador', 'Gestionar roles');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `clave` varchar(255) NOT NULL,
+  `rol` enum('Cliente','Vendedor','Administrador') DEFAULT 'Cliente',
+  `intentos_fallidos` int(11) DEFAULT 0,
+  `bloqueado` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `telefono`, `clave`, `rol`, `intentos_fallidos`, `bloqueado`) VALUES
+(1, 'ariel correa maturana', 'arielcorreama@gmail.com', '3147426653', '$2y$10$5gjRCsyQTq1Y9GciXHj5VugbZmh5StHJoobztLBi1pEo0gFZOjr8G', 'Cliente', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `ventas_vistas_clientes_frecuentes`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `ventas_vistas_clientes_frecuentes` (
+`cliente_id` int(11)
+,`total_compras` bigint(21)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vistas_compras_recientes`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vistas_compras_recientes` (
+`id` int(11)
+,`proveedor_id` int(11)
+,`fecha` date
+,`total` decimal(10,2)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vistas_productos_bajo_stock`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vistas_productos_bajo_stock` (
+`id` int(11)
+,`nombre` varchar(100)
+,`stock` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vistas_productos_mas_vendidos`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vistas_productos_mas_vendidos` (
+`producto_id` int(11)
+,`total_vendido` decimal(32,0)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vistas_proveedores_productos`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vistas_proveedores_productos` (
+`proveedor` varchar(100)
+,`producto` varchar(100)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vistas_ventas_por_clientes`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vistas_ventas_por_clientes` (
+`nombre` varchar(100)
+,`total_gasto` decimal(42,2)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `ventas_vistas_clientes_frecuentes`
+--
+DROP TABLE IF EXISTS `ventas_vistas_clientes_frecuentes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `ventas_vistas_clientes_frecuentes`  AS SELECT `detalles_ventas`.`cliente_id` AS `cliente_id`, count(0) AS `total_compras` FROM `detalles_ventas` GROUP BY `detalles_ventas`.`cliente_id` ORDER BY count(0) DESC ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vistas_compras_recientes`
+--
+DROP TABLE IF EXISTS `vistas_compras_recientes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vistas_compras_recientes`  AS SELECT `registro_compras`.`id` AS `id`, `registro_compras`.`proveedor_id` AS `proveedor_id`, `registro_compras`.`fecha` AS `fecha`, `registro_compras`.`total` AS `total` FROM `registro_compras` ORDER BY `registro_compras`.`fecha` DESC LIMIT 0, 10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vistas_productos_bajo_stock`
+--
+DROP TABLE IF EXISTS `vistas_productos_bajo_stock`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vistas_productos_bajo_stock`  AS SELECT `productos`.`id` AS `id`, `productos`.`nombre` AS `nombre`, `productos`.`stock` AS `stock` FROM `productos` WHERE `productos`.`stock` < 10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vistas_productos_mas_vendidos`
+--
+DROP TABLE IF EXISTS `vistas_productos_mas_vendidos`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vistas_productos_mas_vendidos`  AS SELECT `detalles_ventas`.`producto_id` AS `producto_id`, sum(`detalles_ventas`.`cantidad`) AS `total_vendido` FROM `detalles_ventas` GROUP BY `detalles_ventas`.`producto_id` ORDER BY sum(`detalles_ventas`.`cantidad`) DESC ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vistas_proveedores_productos`
+--
+DROP TABLE IF EXISTS `vistas_proveedores_productos`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vistas_proveedores_productos`  AS SELECT `proveedores`.`nombre` AS `proveedor`, `productos`.`nombre` AS `producto` FROM (`proveedores` join `productos` on(`proveedores`.`id` = `productos`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vistas_ventas_por_clientes`
+--
+DROP TABLE IF EXISTS `vistas_ventas_por_clientes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vistas_ventas_por_clientes`  AS SELECT `clientes`.`nombre` AS `nombre`, sum(`detalles_ventas`.`cantidad` * `detalles_ventas`.`precio`) AS `total_gasto` FROM (`clientes` join `detalles_ventas` on(`clientes`.`id` = `detalles_ventas`.`venta_id`)) GROUP BY `clientes`.`nombre` ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `auditoria`
+--
+ALTER TABLE `auditoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_usuarios` (`usuarios_id`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalles_compra`
+--
+ALTER TABLE `detalles_compra`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalles_ventas`
+--
+ALTER TABLE `detalles_ventas`
+  ADD PRIMARY KEY (`cliente_id`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `registro_compras`
+--
+ALTER TABLE `registro_compras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `roles_permisos`
+--
+ALTER TABLE `roles_permisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `auditoria`
+--
+ALTER TABLE `auditoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalles_compra`
+--
+ALTER TABLE `detalles_compra`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalles_ventas`
+--
+ALTER TABLE `detalles_ventas`
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_compras`
+--
+ALTER TABLE `registro_compras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles_permisos`
+--
+ALTER TABLE `roles_permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `auditoria`
+--
+ALTER TABLE `auditoria`
+  ADD CONSTRAINT `fk_usuarios` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
